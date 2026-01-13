8671df6a977b31d55b75ecbca989ff2a374633f5
runox49 <runeffors@gmail.com>
2026-01-13 21:10:58 +0700

    Update tenniscnx-app.py

--- tenniscnx-app.py
+++ tenniscnx-app.py
@@ -100,7 +100,7 @@ data = [
         "desc_en": "A premium tennis destination with 6 professional hard courts: 3 Indoor (Covered) to avoid sun/rain and 3 Outdoor. Modern facilities with clean showers and a clubhouse.",
         "desc_cn": "清迈高端网球场馆，共 6 片场地：3 片室内遮阳场（防雨防晒）及 3 片标准室外场。设施极佳，提供专业洗浴和休息会所。",
         "url": "https://www.facebook.com/61583261213526",
-        "location_url": "https://maps.app.goo.gl/V7VfS4zYQeNqR7C88" # 这里建议替换为你实测的 Google Map 链接
+        "location_url": "https://maps.app.goo.gl/Q1M82MNk8y9CPA7A6" # 这里建议替换为你实测的 Google Map 链接
     },
 ]
 
